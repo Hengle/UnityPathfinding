b875e13d3f6021df3c1528094da35455466454e0
KowalikJakub <kowalik.kub@gmail.com>
2018-01-31 17:46:07 +0100

    So far so good

---
+++ DijkstraPlugin/Dijkstra Plugin/Assets/_Scripts/Node.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Node : MonoBehaviour
+{
+    public Vector3 Position;
+
+    [SerializeField]
+    private List<Node> adjecentNodes = new List<Node>();
+
+    [SerializeField]
+    private Material Default;
+    [SerializeField]
+    private Material Highlight;
+    private bool selected = false;
+
+    private Ray ray;
+    private RaycastHit raycastHitInfo;
+    private Node hit;
+
+    private MeshRenderer mesh;
+
+    public void AddAdjecentNode(Node node)
+    {
+        if (node != null)
+        {
+            adjecentNodes.Add(node);
+            return;
+        }
+        Debug.LogError("Node " + node.name + " was null while adding it to adjecent waypoints");
+    }
+
+    private void Awake()
+    {
+        Position = transform.position;
+        mesh = GetComponent<MeshRenderer>();
+    }
+    private void OnMouseDown()
+    {
+        if (NodeManager.Instance.StartNode != null)
+        {
+            NodeManager.Instance.EndNode = this;
+        }
+        else
+        {
+            NodeManager.Instance.StartNode = this;
+        }
+        if (selected)
+        {
+            DefaultMaterial();
+        }
+        else
+        {
+            HighlightMaterial();
+        }
+        selected = !selected;
+    }
+    public void DefaultMaterial()
+    {
+        mesh.material = Default;
+    }
+    public void HighlightMaterial()
+    {
+        mesh.material = Highlight;
+    }
+}
---
+++ DijkstraPlugin/Dijkstra Plugin/Assets/_Scripts/NodeManager.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomBehaviour;
+
+[Serializable]
+public class Edge
+{
+    public Node Start, End;
+    public double Weight;
+    public string Name;
+    public Edge(Node Start, Node End)
+    {
+        this.Start = Start;
+        this.End = End;
+        Weight = (End.Position - Start.Position).magnitude;
+        Name = "Start: " + Start.name + " End: " + End.name;
+    }
+}
+
+[Serializable]
+public class Graph
+{
+    public List<Node> VertexSet;
+    public List<Edge> EdgeSet;
+
+    public int VertexCount
+    {
+        get { return VertexSet.Count; }
+    }
+    public int EdgeCount
+    {
+        get { return EdgeSet.Count; }
+    }
+
+    public Graph() { }
+
+    public Graph(List<Node> VertexSet)
+    {
+        this.VertexSet = VertexSet;
+        EdgeSet = new List<Edge>();
+    }
+
+    public Graph(List<Edge> EdgeSet)
+    {
+        this.EdgeSet = EdgeSet;
+
+        //Initializing the vertex set
+        foreach (Edge edge in EdgeSet)
+        {
+            if (!VertexSet.Contains(edge.Start))
+            {
+                VertexSet.Add(edge.Start);
+            }
+            if (!VertexSet.Contains(edge.End))
+            {
+                VertexSet.Add(edge.End);
+            }
+        }
+    }
+    public void AddEdge(Node start, Node end)
+    {
+        if (VertexSet.Contains(start) && VertexSet.Contains(end))
+        {
+            start.AddAdjecentNode(end);
+            end.AddAdjecentNode(start);
+            //We have to chech if EdgeSet contains the end,start edge
+            Edge edge = new Edge(start, end);
+            EdgeSet.Add(edge);
+        }
+    }
+    public void PrintVertexSet()
+    {
+        foreach (var item in VertexSet)
+        {
+            Debug.Log("Node number: " + item.name);
+        }
+    }
+    public void PrintEdgeSet()
+    {
+        foreach (var item in EdgeSet)
+        {
+            Debug.Log(item.Name);
+        }
+    }
+}
+//1. Click and drag to create an graph edge OK!
+//2. Show all edges on screen as lines maybe on canvas ?
+//3. Calculate the shortest path Dijkstra
+public class NodeManager : SingletonBehaviour<NodeManager>
+{
+    public Camera TagetCamera;
+
+    private Graph graph;
+
+    //Variavbles for adding a new edge
+    private Node startNode;
+    private Node endNode;
+
+    public Graph Graph
+    {
+        get { return graph; }
+
+        set { graph = value; }
+    }
+
+    public Node StartNode
+    {
+        get { return startNode; }
+
+        set { startNode = value; }
+    }
+
+    public Node EndNode
+    {
+        get { return endNode; }
+
+        set { endNode = value; }
+    }
+
+    private void Start()
+    {
+        Node[] nodes = FindObjectsOfType<Node>();
+        List<Node> NodesList = new List<Node>();
+        NodesList.AddRange(nodes);
+
+        //Graph initialization
+        Graph = new Graph(NodesList);
+    }
+
+    private void Update()
+    {
+        if (StartNode != null && EndNode != null)
+        {
+            if (StartNode != EndNode)
+            {
+                Graph.AddEdge(StartNode, EndNode);
+                StartNode.AddAdjecentNode(EndNode);
+                EndNode.AddAdjecentNode(StartNode);
+            }
+            //Clear the references
+            StartNode.DefaultMaterial();
+            EndNode.DefaultMaterial();
+            StartNode = null;
+            EndNode = null;
+        }
+    }
+}
